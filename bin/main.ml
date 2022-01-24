4572f8c6c720746d6dc174ffc25bede3c4b7cda7
glebpatcia <GVPatsiya@domclick.ru>
2022-01-24 23:25:01 +0500

    finish

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,19 @@
 open Daemons
 open Lwt.Syntax
 
+let list = [
+  "https://mock.codes/420";
+  "https://mock.codes/200"
+]
+
+let check_timeout = 3.
+
+let request_timeout = 2.
 let run_tasks () = 
   let daemon =
      Telegram_bot_check_urls_daemon.init
-      ~check_timeout:3.
-      ~timeout:3. ["https://mock.codes/420"] in
+      ~check_timeout
+      ~timeout:request_timeout list in
   let* _ = Lwt.all[
     Telegram_bot_check_urls_daemon.run daemon;
     Telegram_bot_updates_daemon.handle_pings()
